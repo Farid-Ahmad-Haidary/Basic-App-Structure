cce5fc351f1abb454fb0b5e589e45bb1ad69d3c1
Farid Ahmad Haidary <faridahmadhaidary01@gmail.com>
2026-04-07 23:03:35 +0430

    mobile navigation is changed to dynamic

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -81,12 +81,12 @@
             <el-disclosure id="mobile-menu" hidden class="block md:hidden">
                 <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
                     <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
-                    <a href="/" aria-current="page"
-                        class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white">Home</a>
-                    <a href="/about"
-                        class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">About</a>
-                    <a href="/contact"
-                        class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Contact</a>
+                    <x-nav-link href="/" aria-current="page"
+                        :active="request()->is('/')" >Home</x-nav-link>
+                    <x-nav-link href="/about"
+                        :active="request()->is('about')" >About</x-nav-link>
+                    <x-nav-link href="/contact"
+                        :active="request()->is('contact')" >Contact</x-nav-link>
                 </div>
                 <div class="border-t border-white/10 pt-4 pb-3">
                     <div class="flex items-center px-5">
