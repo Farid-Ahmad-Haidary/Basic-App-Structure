add0f0e7d7a5d01ce10618464426afdaa33aa8f7
Farid Ahmad Haidary <faridahmadhaidary01@gmail.com>
2026-04-07 22:51:59 +0430

    New features for the navigation and layout components

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,2 +1,8 @@
-{{--* attributes  takes features of one page to another page!
-<a {{$attributes}}>{{$slot}}</a> --}}
+@props(['active' => false])
+
+<a {{ $attributes }}
+    class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' }}
+     rounded-md px-3 py-2 text-sm font-medium"
+    aria-current="{{ $active ? 'page' : 'false' }}">
+    {{ $slot }}
+</a>
